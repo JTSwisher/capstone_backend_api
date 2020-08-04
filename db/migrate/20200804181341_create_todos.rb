993456c99c33441143a20eb08f33664f4d66a03f
JTSwisher <jefftswisher@gmail.com>
2020-08-04 11:18:38 -0700

    create db schema for users&todos

---
+++ db/migrate/20200804181341_create_todos.rb
@@ -0,0 +1,12 @@
+class CreateTodos < ActiveRecord::Migration[6.0]
+  def change
+    create_table :todos do |t|
+      t.string :title
+      t.text :body
+      t.boolean :complete
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
