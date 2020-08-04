f9d5e8df749046a53eca30661cb0f93b7ea0b79c
JTSwisher <jefftswisher@gmail.com>
2020-08-04 11:21:29 -0700

    add model associations & user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_many :todos
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true
+    has_secure_password
 end
